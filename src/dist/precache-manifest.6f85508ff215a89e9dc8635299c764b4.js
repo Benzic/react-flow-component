3a797fcb268da429a27f25d8677b9b7e8fa5e55a
zhaohuan <zhaohuan@unionbigdata.com>
2020-08-07 16:27:35 +0800

    build

---
+++ src/dist/precache-manifest.6f85508ff215a89e9dc8635299c764b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c67f973d906310dd521fc82394381da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f763cc3626363092d02",
+    "url": "/static/css/main.6329d09b.chunk.css"
+  },
+  {
+    "revision": "bc3c4af2973513cc4e87",
+    "url": "/static/js/2.ecc89307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecc89307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f763cc3626363092d02",
+    "url": "/static/js/main.8888c124.chunk.js"
+  },
+  {
+    "revision": "9bd9356fe37e15cccad0",
+    "url": "/static/js/runtime-main.690e4320.js"
+  }
+]);
